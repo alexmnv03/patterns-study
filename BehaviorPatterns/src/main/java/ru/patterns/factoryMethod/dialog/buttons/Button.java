bfaf66b15573736cacd372f0518b2fe387e8c6ca
alex_man <alexmnsbs@gmail.com>
2025-10-23 01:54:33 +0300

    Patterns-FactoryMethod-code-ref

--- BehaviorPatterns/src/main/java/ru/patterns/factoryMethod/dialog/buttons/Button.java
+++ BehaviorPatterns/src/main/java/ru/patterns/factoryMethod/dialog/buttons/Button.java
@@ -3,5 +3,5 @@ package ru.patterns.factoryMethod.dialog.buttons;
 public interface Button {
   void render();
   void onClick();
-  int outSize();
+  void outSize();
 }
--- BehaviorPatterns/src/main/java/ru/patterns/factoryMethod/dialog/buttons/HtmlButton.java
+++ BehaviorPatterns/src/main/java/ru/patterns/factoryMethod/dialog/buttons/HtmlButton.java
@@ -1,6 +1,6 @@
 package ru.patterns.factoryMethod.dialog.buttons;
 
-public class HtmlButton implements Button {
+public class HtmlButton extends AbstractButton implements Button {
 
   public void render() {
     System.out.println("<button>Test HtmlButton</button>");
@@ -12,7 +12,15 @@ public class HtmlButton implements Button {
   }
 
   @Override
-  public int outSize() {
-    return 0;
+  public void outSize() {
+    System.out.println("Size HtmlButton = " + getSize());
   }
+
+  public HtmlButton() {
+  }
+
+  public HtmlButton(int size) {
+    setSize(size);
+  }
+
 }
