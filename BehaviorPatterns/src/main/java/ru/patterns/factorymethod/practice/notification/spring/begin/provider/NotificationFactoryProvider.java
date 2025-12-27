1f087bdef886b33649fe61ce400dcc1707c177e2
alex_man <alexmnsbs@gmail.com>
2025-12-27 05:42:02 +0300

    Patterns-FactoryMethod-dynamic-spring-provider

---
+++ BehaviorPatterns/src/main/java/ru/patterns/factorymethod/practice/notification/spring/begin/provider/NotificationFactoryProvider.java
@@ -0,0 +1,21 @@
+package ru.patterns.factorymethod.practice.notification.spring.begin.provider;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.patterns.factorymethod.practice.notification.simple.factory.NotificationFactory;
+
+@Component
+public class NotificationFactoryProvider {
+
+  private final Map<String, NotificationFactory> factories;
+
+  @Autowired
+  public NotificationFactoryProvider(Map<String, NotificationFactory> factories) {
+    this.factories = factories;
+  }
+
+  public NotificationFactory getFactory(String type) {
+    return factories.getOrDefault(type.toUpperCase(), factories.get("smsF"));
+  }
+}
