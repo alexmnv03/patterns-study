314fbf95ccd71cbd0a71c7830ef6983da7ac2a32
alex_man <alexmnsbs@gmail.com>
2026-01-01 01:09:57 +0300

    Patterns-FactoryMethod-dynamic-spring-resources

---
+++ BehaviorPatterns/src/main/java/ru/patterns/factorymethod/practice/notification/spring/restver/NoticeRestApp.java
@@ -0,0 +1,12 @@
+package ru.patterns.factorymethod.practice.notification.spring.restver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeRestApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoticeRestApp.class, args);
+  }
+}
